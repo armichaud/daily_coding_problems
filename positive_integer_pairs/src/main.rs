5d54ed8559e8fc388bc68419012b9794b82daddb
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-08-03 13:19:05 -0400

    Positive Integer Pairs

---
+++ positive_integer_pairs/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: positive_integer_pairs <integer> <integer>");
+        return;
+    }
+    let mut pairs = Vec::<(i32, i32)>::new();
+    let m = args[1].parse::<i32>().expect("Must be an integer");
+    let n = args[2].parse::<i32>().expect("Must be an integer");
+    
+    let half = m / 2;
+
+    for a in 1..half {
+        let b = m - a;
+        if a ^ b == n {
+            pairs.push((a, b));
+        }
+    }
+    println!("Pairs: {:?}", pairs);
+}
