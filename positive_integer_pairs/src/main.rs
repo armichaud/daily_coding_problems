70922543aaf64f05881f4b4f869bd3d50893e82d
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-08-03 13:20:07 -0400

    Comment

--- positive_integer_pairs/src/main.rs
+++ positive_integer_pairs/src/main.rs
@@ -1,3 +1,9 @@
+/*
+ * Given integers M and N, write a program that counts how many positive integer pairs (a, b) satisfy the following conditions:
+ * a + b = M
+ * a XOR b = N 
+ */
+
 use std::env;
 
 fn main() {
